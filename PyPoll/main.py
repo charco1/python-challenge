e2beb08b60252e0640ca184ccd7f21d440e0eb97
Courtney <katrice70815@yahoo.com>
2018-07-17 23:55:39 -0500

    commit

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+import os
+import csv
+
+#file to load
+election_path = os.path.join("Resources","election_data.csv")
+
+#Track paramaters for calculations
+total_votes = 0
+
+#   for rows in csvreader:
+total_votes = total_votes + 1
+print(total_votes)
